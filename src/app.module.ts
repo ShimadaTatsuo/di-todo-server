c978d115276aa33690a93ace69018a28e2867cb6
島田龍男 <tatsuo.shimada39@gmail.com>
2024-02-20 18:10:05 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'secret',
+      database: 'mydb-dev',
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, DeleteResult, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private dataSource: DataSource,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return 'This action adds a new user';
+  }
+
+  async createMany(users: User[]) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.save(users[0]);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: number): Promise<DeleteResult> {
+    return await this.userRepository.delete(id);
+  }
+}
